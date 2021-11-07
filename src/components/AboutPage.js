cbe83695cb5362bcc8c393d744bc8e5b5f25293d
playstore777 <67255734+playstore777@users.noreply.github.com>
2021-11-07 16:49:20 +0530

    updated info and images

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -70,9 +70,7 @@ export const AboutPage = () => {
                     simple yet beautiful websites with great user experience.
                     <br /> <br />
                     I'm interested in the whole frontend stack Like trying new
-                    things and building great projects. I'm an independent
-                    freelancer and blogger. I love to write blogs and read
-                    books.
+                    things and building great projects. I love to read books.
                     <br /> <br />I believe everything is an Art when you put
                     your consciousness in it. You can connect with me via social
                     links.
